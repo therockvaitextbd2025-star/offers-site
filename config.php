ddd5fae4c5c795321ced23a5d3ad8fe1e7692301
therockvaitextbd2025-star <therockvai.textbd2025@gmail.com>
2026-03-12 10:46:27 +0600

    Add configuration file for environment variables

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$supabaseUrl = getenv("SUPABASE_URL");
+$supabaseKey = getenv("SUPABASE_KEY");
+
+$cpagripKey = getenv("CPAGRIP_PUBKEY");
+$ogadsKey   = getenv("OGADS_KEY");
+$adgateKey  = getenv("ADGATE_KEY");
+$adscendKey = getenv("ADSCEND_KEY");
+
+$CACHE_TIME = 432000; // 5 days
