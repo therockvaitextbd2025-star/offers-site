04c72851ca8249f190ff50c3168520346dd85a72
therockvaitextbd2025-star <therockvai.textbd2025@gmail.com>
2026-03-12 10:47:35 +0600

    Add load_offers function for fetching offers
    Implement load_offers function to fetch and filter offers from CPAGrip and OGAds APIs.

---
+++ engine.php
@@ -0,0 +1,99 @@
+<?php
+
+function load_offers($country,$user_id){
+
+global $cpagripKey,$ogadsKey;
+
+$offers=[];
+
+/* CPAGrip */
+
+$feed=file_get_contents(
+"https://www.cpagrip.com/common/offer_feed_json.php?pubkey=$cpagripKey&country=$country&tracking_id=$user_id"
+);
+
+$data=json_decode($feed,true);
+
+if(isset($data['offers'])){
+
+foreach($data['offers'] as $o){
+
+$offers[]=[
+"title"=>$o['title'],
+"link"=>$o['offerlink'],
+"payout"=>$o['payout'],
+"image"=>$o['thumbnail']
+];
+
+}
+
+}
+
+/* OGAds */
+
+if($ogadsKey){
+
+$feed2=@file_get_contents(
+"https://api.ogads.com/v1/offers?api_key=$ogadsKey&country=$country"
+);
+
+$data2=json_decode($feed2,true);
+
+if(isset($data2['offers'])){
+
+foreach($data2['offers'] as $o){
+
+$offers[]=[
+"title"=>$o['name'],
+"link"=>$o['link'],
+"payout"=>$o['payout'],
+"image"=>$o['image']
+];
+
+}
+
+}
+
+}
+
+/* FILTER */
+
+$filtered=[];
+
+foreach($offers as $o){
+
+if($o['payout'] < 0.10) continue;
+
+if(empty($o['link'])) continue;
+
+$filtered[]=$o;
+
+}
+
+/* REMOVE DUPLICATE */
+
+$unique=[];
+
+foreach($filtered as $o){
+
+$key=md5($o['title']);
+
+$unique[$key]=$o;
+
+}
+
+$filtered=array_values($unique);
+
+/* SORT */
+
+usort($filtered,function($a,$b){
+
+return $b['payout'] <=> $a['payout'];
+
+});
+
+/* LIMIT */
+
+return array_slice($filtered,0,50);
+
+}
