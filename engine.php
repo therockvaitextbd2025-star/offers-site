9445e7fd00996c30cd515109c6c3ae27451edcea
therockvaitextbd2025-star <therockvai.textbd2025@gmail.com>
2026-03-13 11:42:36 +0600

    Update engine.php

--- engine.php
+++ engine.php
@@ -4,9 +4,9 @@ function load_offers($country, $user_id) {
     global $cpagripKey, $ogadsKey;
     $offers = [];
 
-    // Place this link inside your engine.php file:
-$url = "https://www.cpagrip.com/common/offer_feed_json.php?user_id=2441114&key=7f0b09da3b2d682f1189d1d6abbf24fc&country=$country&tracking_id=$user_id&showall=1";
-    
+        /* CPAGrip - Using cURL for better reliability */
+    $url = "https://www.cpagrip.com/common/offer_feed_json.php?user_id=2441114&key=7f0b09da3b2d682f1189d1d6abbf24fc&country=$country&tracking_id=$user_id&showall=1";
+
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
