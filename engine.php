862ac588cdc4655cf3eac6221e77c4ae670c5045
therockvaitextbd2025-star <therockvai.textbd2025@gmail.com>
2026-03-14 13:28:59 +0600

    Update engine.php

--- engine.php
+++ engine.php
@@ -1,4 +1,10 @@
+
 <?php
+// Global Cache Control for all pages using this engine
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 function get_user_country($user_id) {
     $supabaseUrl = getenv('SUPABASE_URL');
     $supabaseKey = getenv('SUPABASE_KEY');
