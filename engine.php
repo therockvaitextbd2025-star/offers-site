d2b7b600312afdeb14216a509e90a7e5fa76ce5f
therockvaitextbd2025-star <therockvai.textbd2025@gmail.com>
2026-04-04 10:51:21 +0600

    Add CORS headers for API access
    Added CORS headers to allow cross-origin requests.

--- engine.php
+++ engine.php
@@ -3,6 +3,10 @@
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
 
 // কমন ডাটা রিকোয়েস্ট ফাংশন
 function call_supabase($url) {
